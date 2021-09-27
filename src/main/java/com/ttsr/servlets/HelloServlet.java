f851150b76924c0a5aaab66618e4043d9fb5b746
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2021-09-27 16:36:45 +0700

    homework 6 - apache maven, servlets

---
+++ src/main/java/com/ttsr/servlets/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.ttsr.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        try(PrintWriter printWriter = response.getWriter()){
+            printWriter.println("<h1>HelloWorld</h1>");
+        }
+    }
+}
